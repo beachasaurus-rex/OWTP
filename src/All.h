738cd715bf36c46b9a910e3624b6e448e2fc49ac
jonathan-rizk <jonrizk@protonmail.com>
2019-12-13 19:59:11 -0600

    removed the dots

--- src/All.h
+++ src/All.h
@@ -1,19 +1,19 @@
 #ifndef REGION1_H
     #define REGION1_H
-    #include "./Regions/Region1.h"
+    #include "Regions/Region1.h"
 #endif
 
 #ifndef REGION2_H
     #define REGION2_H
-    #include "./Regions/Region2.h"
+    #include "Regions/Region2.h"
 #endif
 
 #ifndef REGION3_H
     #define REGION3_H
-    #include "./Regions/Region3.h"
+    #include "Regions/Region3.h"
 #endif
 
 #ifndef R2_R3_BOUNDS_H
     #define R2_R3_BOUNDS_H
-    #include "./Regions/Boundaries/RegionBoundaries/R2andR3.h"
+    #include "Regions/Boundaries/RegionBoundaries/R2andR3.h"
 #endif
