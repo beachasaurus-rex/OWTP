6c6f69d2a2e751334a40417612f20c9d783ed057
jonathan-rizk <jonrizk@protonmail.com>
2019-12-13 22:35:39 -0600

    ci stuff

--- ci/build.sh
+++
@@ -1,3 +0,0 @@
-CONFIG="Debug"
-cmake -G"Visual Studio 16 2019" -Bbuild -H.. -DCMAKE_CONFIGURATION_TYPES=${CONFIG}
-cmake --build build --config ${CONFIG}
---
+++ cibuild.sh
@@ -0,0 +1,3 @@
+CONFIG="Debug"
+cmake -G"Visual Studio 16 2019" -Bbuild -H. -DCMAKE_CONFIGURATION_TYPES=${CONFIG}
+cmake --build build --config ${CONFIG}
