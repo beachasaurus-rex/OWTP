dae2b2e916c8252468626bf965c9fe3d49e1e438
jonathan-rizk <jonrizk@protonmail.com>
2020-01-09 21:04:56 -0600

    test

--- ci/build.sh
+++ ci/build.sh
@@ -1,5 +1,3 @@
 CONFIG="Debug"
-BUILDIR="build"
-SRCDIR=".."
-cmake -G"Visual Studio 16 2019" -B${BUILDIR} -H${SRCDIR} -DCMAKE_CONFIGURATION_TYPES=${CONFIG}
-cmake --build ${BUILDIR} --config ${CONFIG}
+cmake -G"Visual Studio 16 2019" -Bbuild -H. -DCMAKE_CONFIGURATION_TYPES=${CONFIG}
+cmake --build build --config ${CONFIG}
