334cd004727959b16a3ef877dd175ad9d25477fe
jonathan-rizk <jonrizk@protonmail.com>
2020-01-19 09:05:41 -0600

    test

--- ci/after_test.ps1
+++ ci/after_test.ps1
@@ -2,4 +2,4 @@ $test_results_path = ".\TestRunner_results.xml"
 $wc = new-object net.webclient
 $endpoint = "https://ci.appveyor.com/api/testresults/junit/$($env:APPVEYOR_JOB_ID)"
 
-$(wc).UploadFile($(endpoint), (Resolve-Path $(test_results_path)))
+$wc.UploadFile($(endpoint), (Resolve-Path $(test_results_path)))
