e6a82f51a736ed6bda9d8cd14baa8c45159401a0
jonathan-rizk <jonrizk@protonmail.com>
2020-01-25 17:01:25 -0600

    restructured temp subregion boundaries

--- src/Regions/Boundaries/SubregionBoundaries/Region3/Boundaries_vTP.h
+++ src/Regions/Boundaries/SubregionBoundaries/Region3/Boundaries_vTP.h
@@ -8,224 +8,178 @@
 	#include "..\..\..\ReferenceConstants.h"
 #endif
 
-//All Boundary Equations for v(T,P)
+//All Subregion Boundary Equations for v(T,P)
 
 
-
-//Constants/functions used for all boundary equations
-
-//K
-static const int _tStar_T3 = 1;
-
-static double _theta_T3(double temp)
-{
-	return temp / _tStar_T3;
-}
-static double _pi_T3_subregs_v_T_P(double press)
-{
-	//kPa
-	const int _pStar_T3_subregs_v_T_P = 1000;
-
-	return press / _pStar_T3_subregs_v_T_P;
-}
 
 //Iteration Constants R3ab
 
-static const int _i_v_T_P_R3ab[5] = { 1, 2, 3, 4, 5 };
 static const int _I_v_T_P_R3ab[5] = { 0, 1, 2, -1, -2 };
 static const double _n_v_T_P_R3ab[5] = { 1547.93642129415, -187.661219490113, 21.3144632222113, -1918.87498864292, 918.419702359447 };
 
 //Iteration Constants R3cd
 
-static const int _i_v_T_P_R3cd[4] = { 1, 2, 3, 4 };
 static const int _I_v_T_P_R3cd[4] = { 0, 1, 2, 3 };
 static const double _n_v_T_P_R3cd[4] = { 585.276966696349, 2.78233532206915, -1.27283549295878E-02, 1.59090746562729E-04 };
 
 //Iteration Constants R3gh
 
-static const int _i_v_T_P_R3gh[5] = { 1, 2, 3, 4, 5 };
 static const int _I_v_T_P_R3gh[5] = { 0, 1, 2, 3, 4 };
 static const double _n_v_T_P_R3gh[5] = { -24928.4240900418, 4281.43584791546, -269.02917314013, 7.51608051114157, -7.87105249910383E-02 };
 
 //Iteration Constants R3ij
 
-static const int _i_v_T_P_R3ij[5] = { 1, 2, 3, 4, 5 };
 static const int _I_v_T_P_R3ij[5] = { 0, 1, 2, 3, 4 };
 static const double _n_v_T_P_R3ij[5] = { 584.814781649163, -0.616179320924617, 0.260763050899562, -5.87071076864459E-03, 5.15308185433082E-05 };
 
 //Iteration Constants R3jk
 
-static const int _i_v_T_P_R3jk[5] = { 1, 2, 3, 4, 5 };
 static const int _I_v_T_P_R3jk[5] = { 0, 1, 2, 3, 4 };
 static const double _n_v_T_P_R3jk[5] = { 617.229772068439, -7.70600270141675, 0.697072596851896, -1.57391839848015E-02, 1.37897492684194E-04 };
 
 //Iteration Constants R3mn
 
-static const int _i_v_T_P_R3mn[5] = { 1, 2, 3, 4 };
 static const int _I_v_T_P_R3mn[5] = { 0, 1, 2, 3 };
 static const double _n_v_T_P_R3mn[5] = { 535.339483742384, 7.61978122720128, -0.158365725441648, 1.92871054508108E-03 };
 
 //Iteration Constants R3op
 
-static const int _i_v_T_P_R3op[5] = { 1, 2, 3, 4, 5 };
 static const int _I_v_T_P_R3op[13] = { 0, 1, 2, -1, -2 };
 static const double _n_v_T_P_R3op[13] = { 969.461372400213, -332.500170441278, 64.2859598466067, 773.845935768222, -1523.13732937084 };
 
 //Iteration Constants R3qu
 
-static const int _i_v_T_P_R3qu[4] = { 1, 2, 3, 4 };
 static const int _I_v_T_P_R3qu[12] = { 0, 1, 2, 3 };
 static const double _n_v_T_P_R3qu[12] = { 565.603648239126, 5.29062258221222, -0.102020639611016, 1.22240301070145E-03 };
 
 //Iteration Constants R3rx
 
-static const int _i_v_T_P_R3rx[4] = { 1, 2, 3, 4 };
 static const int _I_v_T_P_R3rx[4] = { 0, 1, 2, 3 };
 static const double _n_v_T_P_R3rx[4] = { 584.561202520006, -1.02961025163669, 0.243293362700452, -2.94905044740799E-03 };
 
+//Generic boundary equation for all subregions except ab, ef, op
+
+static double _T3_v_T_P(double press, const int* IArr, const double* nArr, int arrSize)
+{
+	//K
+	const double tStar = 1;
+	//kPa
+	const double pStar = 1000;
+
+	double pi = press / pStar;
+	double sum = 0;
+	for (int i = 0; i < arrSize; i++)
+	{
+		double calc = nArr[i] * pow(pi, IArr[i]);
+		sum = sum + calc;
+	}
+	return sum * tStar;
+}
+
+//Generic boundary equation for subregions ab, op
+
+static double _T3_ab_op_v_T_P(double press, const int* IArr, const double* nArr,
+	int arrSize)
+{
+	//K
+	const double tStar = 1;
+	//kPa
+	const double pStar = 1000;
+
+	double pi = press / pStar;
+	double sum = 0;
+	for (int i = 0; i < arrSize; i++)
+	{
+		double calc = nArr[i] * pow(log(pi), IArr[i]);
+		sum = sum + calc;
+	}
+	return sum * tStar;
+}
+
 //Boundary Equations R3ab
 
-double _T3ab_v_T_P(double pressure)
+double _T3ab_v_T_P(double press)
 {
-	double pi = _pi_T3_subregs_v_T_P(pressure);
-
-	double sum = 0;
-	for (int i = 0; i < ITERCONST(_i_v_T_P_R3ab); i++)
-	{
-		sum = sum + _n_v_T_P_R3ab[i] * pow(log(pi), _I_v_T_P_R3ab[i]);
-	}
-
-	return sum * _tStar_T3;
+	int N = ITERCONST(_I_v_T_P_R3ab);
+	return _T3_ab_op_v_T_P(press, _I_v_T_P_R3ab, _n_v_T_P_R3ab, N);
 }
 
 //Boundary Equations R3cd
 
-double _T3cd_v_T_P(double pressure)
+double _T3cd_v_T_P(double press)
 {
-	double pi = _pi_T3_subregs_v_T_P(pressure);
-
-	double sum = 0;
-	for (int i = 0; i < ITERCONST(_i_v_T_P_R3cd); i++)
-	{
-		sum = sum + _n_v_T_P_R3cd[i] * pow(pi, _I_v_T_P_R3cd[i]);
-	}
-
-	return sum * _tStar_T3;
+	int N = ITERCONST(_I_v_T_P_R3cd);
+	return _T3_v_T_P(press, _I_v_T_P_R3cd, _n_v_T_P_R3cd, N);
 }
 
 //Boundary Equations R3ef
 
-double _T3ef_v_T_P(double pressure)
+double _T3ef_v_T_P(double press)
 {
-	const double thetaSat = 3.727888004;
-	double pi = _pi_T3_subregs_v_T_P(pressure);
-	return _tStar_T3 *
-		(thetaSat * (pi - 22.064) + 647.096);
+	const double dPiSat_dPi = 3.727888004;
+	//K
+	const double tStar = 1;
+	//kPa
+	const double pStar = 1000;
+
+	double pi = press / pStar;
+	return tStar * (dPiSat_dPi * (pi - 22.064) + 647.096);
 }
 
 //Boundary Equations R3gh
 
-double _T3gh_v_T_P(double pressure)
+double _T3gh_v_T_P(double press)
 {
-	double pi = _pi_T3_subregs_v_T_P(pressure);
-
-	double sum = 0;
-	for (int i = 0; i < ITERCONST(_i_v_T_P_R3gh); i++)
-	{
-		sum = sum + _n_v_T_P_R3gh[i] * pow(pi, _I_v_T_P_R3gh[i]);
-	}
-
-	return sum * _tStar_T3;
+	int N = ITERCONST(_I_v_T_P_R3gh);
+	return _T3_v_T_P(press, _I_v_T_P_R3gh, _n_v_T_P_R3gh, N);
 }
 
 //Boundary Equations R3ij
 
-double _T3ij_v_T_P(double pressure)
+double _T3ij_v_T_P(double press)
 {
-	double pi = _pi_T3_subregs_v_T_P(pressure);
-
-	double sum = 0;
-	for (int i = 0; i < ITERCONST(_i_v_T_P_R3ij); i++)
-	{
-		sum = sum + _n_v_T_P_R3ij[i] * pow(pi, _I_v_T_P_R3ij[i]);
-	}
-
-	return sum * _tStar_T3;
+	int N = ITERCONST(_I_v_T_P_R3ij);
+	return _T3_v_T_P(press, _I_v_T_P_R3ij, _n_v_T_P_R3ij, N);
 }
 
 //Boundary Equations R3jk
 
-double _T3jk_v_T_P(double pressure)
+double _T3jk_v_T_P(double press)
 {
-	double pi = _pi_T3_subregs_v_T_P(pressure);
-
-	double sum = 0;
-	for (int i = 0; i < ITERCONST(_i_v_T_P_R3jk); i++)
-	{
-		sum = sum + _n_v_T_P_R3jk[i] * pow(pi, _I_v_T_P_R3jk[i]);
-	}
-
-	return sum * _tStar_T3;
+	int N = ITERCONST(_I_v_T_P_R3jk);
+	return _T3_v_T_P(press, _I_v_T_P_R3jk, _n_v_T_P_R3jk, N);
 }
 
 //Boundary Equations R3mn
 
-double _T3mn_v_T_P(double pressure)
+double _T3mn_v_T_P(double press)
 {
-	double pi = _pi_T3_subregs_v_T_P(pressure);
-
-	double sum = 0;
-	for (int i = 0; i < ITERCONST(_i_v_T_P_R3mn); i++)
-	{
-		sum = sum + _n_v_T_P_R3mn[i] * pow(pi, _I_v_T_P_R3mn[i]);
-	}
-
-	return sum * _tStar_T3;
+	int N = ITERCONST(_I_v_T_P_R3mn);
+	return _T3_v_T_P(press, _I_v_T_P_R3mn, _n_v_T_P_R3mn, N);
 }
 
 //Boundary Equations R3op
 
-double _T3op_v_T_P(double pressure)
+double _T3op_v_T_P(double press)
 {
-	double pi = _pi_T3_subregs_v_T_P(pressure);
-
-	double sum = 0;
-	for (int i = 0; i < ITERCONST(_i_v_T_P_R3op); i++)
-	{
-		sum = sum + _n_v_T_P_R3op[i] * pow(log(pi), _I_v_T_P_R3op[i]);
-	}
-
-	return sum * _tStar_T3;
+	int N = ITERCONST(_I_v_T_P_R3op);
+	return _T3_ab_op_v_T_P(press, _I_v_T_P_R3op, _n_v_T_P_R3op, N);
 }
 
 //Boundary Equations R3qu
 
-double _T3qu_v_T_P(double pressure)
+double _T3qu_v_T_P(double press)
 {
-	double pi = _pi_T3_subregs_v_T_P(pressure);
-
-	double sum = 0;
-	for (int i = 0; i < ITERCONST(_i_v_T_P_R3qu); i++)
-	{
-		sum = sum + _n_v_T_P_R3qu[i] * pow(pi, _I_v_T_P_R3qu[i]);
-	}
-
-	return sum * _tStar_T3;
+	int N = ITERCONST(_I_v_T_P_R3qu);
+	return _T3_v_T_P(press, _I_v_T_P_R3qu, _n_v_T_P_R3qu, N);
 }
 
 //Boundary Equations R3rx
 
-double _T3rx_v_T_P(double pressure)
+double _T3rx_v_T_P(double press)
 {
-	double pi = _pi_T3_subregs_v_T_P(pressure);
-
-	double sum = 0;
-	for (int i = 0; i < ITERCONST(_i_v_T_P_R3rx); i++)
-	{
-		sum = sum + _n_v_T_P_R3rx[i] * pow(pi, _I_v_T_P_R3rx[i]);
-	}
-
-	return sum * _tStar_T3;
+	int N = ITERCONST(_I_v_T_P_R3rx);
+	return _T3_v_T_P(press, _I_v_T_P_R3rx, _n_v_T_P_R3rx, N);
 }
 
 
