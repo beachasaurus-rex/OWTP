0542ae7a54e00a6097811da72bb060e390b8e679
jonathan-rizk <jonrizk@protonmail.com>
2020-03-26 19:31:19 -0500

    cmake os if

--- src/Export.c
+++ src/Export.c
@@ -6,7 +6,7 @@
 #ifdef _WIN32
     #define DLLEXPORT __declspec(dllexport)
 #else
-    #define DLLEXPORT __attribute__
+    #define DLLEXPORT __attribute__((visibility("default")))
 #endif
 
 #if _DEBUG
